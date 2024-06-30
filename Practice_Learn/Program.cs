dd1cc0bdffad4ad9a76683590a677f0f20984273
XCOOPER1 <68756206+XCOOPER1@users.noreply.github.com>
2024-06-29 21:32:10 -0500

    Add files via upload

---
+++ Practice_Learn/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Security.Authentication;
+
+namespace Basics
+{
+    class Learning
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+
+           int targetNumber = random.Next(1, 101);
+
+           int attempts = 0;
+             
+           bool gameOver = false;
+
+            Console.WriteLine("Welcome to the Guessing Game!");
+            Console.WriteLine("I'm thinking of a number between 1 and  100. What is it?");
+
+            while (!gameOver)
+            {
+
+                Console.WriteLine("Enter your guess: ");
+                String playerGuess = Console.ReadLine();
+                int guess = Convert.ToInt32(playerGuess);
+
+                attempts++;
+                
+                if (guess == targetNumber)
+                {
+                    Console.WriteLine("Congrat's! You win " + "You guess the number in " + attempts + " attemps.");
+
+                    gameOver = true;
+
+                }
+                else if (guess < targetNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Too high!");
+                }
+            }
+
+            Console.WriteLine("Thanks for playing the Guessing Game!");
+
+            Console.ReadKey();
+        }
+    }
+}
